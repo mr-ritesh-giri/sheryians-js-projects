af618ab5887f67b2681fcc6d3f5496c518b6acdc
Ritesh Giri <rrgiri032@gmail.com>
2024-03-23 20:19:44 +0530

    //

--- Multiple Image Hover/script.js
+++ Multiple Image Hover/script.js
@@ -1,15 +1,15 @@
-let elem = document.querySelectorAll(".elem");
-let elemImg = document.querySelectorAll(".elem img");
+const elem = document.querySelectorAll(".elem");
 
 elem.forEach(function (val) {
   val.addEventListener("mouseenter", function () {
     val.childNodes[3].style.opacity = 1;
   });
-  val.addEventListener("mouseleave", function () {
-    val.childNodes[3].style.opacity = 0;
-  });
+
   val.addEventListener("mousemove", function (dets) {
     val.childNodes[3].style.left = dets.x - 100 + "px";
-    val.childNodes[3].style.top = dets.y - 100 + "px";
+  });
+
+  val.addEventListener("mouseleave", function () {
+    val.childNodes[3].style.opacity = 0;
   });
 });
